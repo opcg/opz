6890aca865068c6e08dcc3563bce99b08d57932b
Christopher Heckey <chrisheckey@gmail.com>
2019-06-20 08:48:16 -0700

    fixing python compilation errors

--- src/python/opz/hello.py
+++ src/python/opz/hello.py
@@ -2,3 +2,12 @@
 
 def world():
     print('Hello World!')
+
+
+class Example:
+    def __init__(self, msg: str):
+        self.msg = msg
+        print('Example constructor with msg:', self.msg)
+
+    def getMsg(self):
+        return self.msg
