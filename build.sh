9c6778606506149b0e2f49049c09ceef4d177de6
Christopher Heckey <chrisheckey@gmail.com>
2019-07-23 08:05:16 -0700

    update rez, rezconfig loading

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ export SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 echo "Remove ${SCRIPTPATH}/build/apps/opz/debug"
 rm -rf ${SCRIPTPATH}/build/apps/opz/debug
 
-echo "Remove ${SCRIPTPATH}/build/target/x86_64-apple-darwin/pyoxidizer"
-rm -rf ${SCRIPTPATH}/build/target/x86_64-apple-darwin/pyoxidizer
+echo "Remove ${SCRIPTPATH}/build/target/x86_64-apple-darwin"
+rm -rf ${SCRIPTPATH}/build/target/x86_64-apple-darwin
 
 pyoxidizer build
